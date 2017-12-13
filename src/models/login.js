8d6cce6ab70c773d8db50291ecd6da99cccf51b7
quan.wang <quan.wang@weimob.com>
2017-12-13 19:02:57 +0800

    登录页面输入用户名和密码不校验

--- src/models/login.js
+++ src/models/login.js
@@ -10,11 +10,13 @@ export default {
     * login ({
       payload,
     }, { put, call, select }) {
-      const data = yield call(login, payload)
+      // let data = yield call(login, payload)
+      let data = {}
+      data.success = true
       const { locationQuery } = yield select(_ => _.app)
       if (data.success) {
         const { from } = locationQuery
-        yield put({ type: 'app/query' })
+        // yield put({ type: 'app/query' })
         if (from && from !== '/login') {
           yield put(routerRedux.push(from))
         } else {
